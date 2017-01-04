53c09cb80deab04958f32825ef8a0bfa06e2b65d
BlueWhiteSoul <matrixofprime@gmail.com>
2017-01-04 20:10:45 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 #include <time.h>
 #include "AOP.h"
 #include <functional>
+#include"Quantity.h"
+using std::cout;
+using std::endl;
 template<class...Args>
 struct used_time
 {
@@ -11,20 +14,27 @@ struct used_time
 	}
 	void After(Args...args)
 	{
-		std::cout << static_cast<double>(clock() - m_time) / CLOCKS_PER_SEC << std::endl;
+		cout << static_cast<double>(clock() - m_time) / CLOCKS_PER_SEC << endl;
 	}
 	clock_t m_time;
 };
 
 int main()
 {
-
 	auto f = [](int a)
 	{
-		std::cout << a << std::endl;
+		cout << a << std::endl;
 	};
-
 	Invoke<used_time<int>,used_time<float> >(f, 1);
+	
+	Meter radius(5);
+	Meter height(10);
+	Kilogram mass(400);
+	auto volume = PI*radius*radius*height;
+	auto density = mass / volume;
+	cout << volume.value() << endl;
+	cout << density.value() << endl;
+	
 	system("pause");
 	return 0;
 }
